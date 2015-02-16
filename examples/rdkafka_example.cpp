9afc74fb51eb99a3d7dac1613e3cdf21c0a5c972
Magnus Edenhill <magnus@edenhill.se>
2015-02-16 13:26:48 +0100

    Renamed RdKafka::Producer::MSG_COPY and MSG_FREE to RK_MSG_COPY and RK_MSG_FREE (issue #198)
    MSG_COPY and MSG_FREE are left in place for backwards compatibility, unless
    MSG_FREE is defined because of sys/msg.h inclusion.

--- examples/rdkafka_example.cpp
+++ examples/rdkafka_example.cpp
@@ -367,7 +367,7 @@ int main (int argc, char **argv) {
        */
       RdKafka::ErrorCode resp =
 	producer->produce(topic, partition,
-			  RdKafka::Producer::MSG_COPY /* Copy payload */,
+			  RdKafka::Producer::RK_MSG_COPY /* Copy payload */,
 			  const_cast<char *>(line.c_str()), line.size(),
 			  NULL, NULL);
       if (resp != RdKafka::ERR_NO_ERROR)

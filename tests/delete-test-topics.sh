29fbde9ea3512e17d290420f2e50ec885f175179
Magnus Edenhill <magnus@edenhill.se>
2015-08-29 13:35:41 +0200

    Betterized delete-test-topics.sh

--- tests/delete-test-topics.sh
+++ tests/delete-test-topics.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #
 
-set -e
+set -eu
+
+if [[ "$1" == "-n" ]]; then
+    DO_DELETE=0
+    shift
+else
+    DO_DELETE=1
+fi
 
 ZK=$1
 KATOPS=$2
@@ -12,16 +19,21 @@ if [[ -z "$RE" ]]; then
 fi
 
 if [[ -z "$KATOPS" ]]; then
-    echo "Usage: $0 <zookeeper-address> <kafka-topics.sh> [<topic-name-regex>]"
+    echo "Usage: $0 [-n] <zookeeper-address> <kafka-topics.sh> [<topic-name-regex>]"
     echo ""
     echo "Deletes all topics matching regex $RE"
+    echo ""
+    echo "  -n  - Just collect, dont actually delete anything"
     exit 1
 fi
 
 
 echo -n "Collecting list of matching topics... "
-TOPICS=$($KATOPS --zookeeper $ZK --list 2>/dev/null | grep "$RE")
-echo "${#TOPICS} topics found"
+TOPICS=$($KATOPS --zookeeper $ZK --list 2>/dev/null | grep "$RE") || true
+N_TOPICS=$(echo "$TOPICS" | wc -w)
+echo "$N_TOPICS topics found"
+
+[[ $DO_DELETE != 1 ]] && exit 0
 
 for t in $TOPICS; do
     echo -n "Deleting topic $t... "
@@ -31,7 +43,9 @@ done
 echo "Done"
 
 echo -n "Collecting list of matching topics... "
-TOPICS=$($KATOPS --zookeeper $ZK --list 2>/dev/null | grep "$RE")
-echo "${#TOPICS} topics found"
+TOPICS=$($KATOPS --zookeeper $ZK --list 2>/dev/null | grep "$RE") || true
+N_TOPICS=$(echo "$TOPICS" | wc -w)
+echo "$N_TOPICS topics found"
+
 
     

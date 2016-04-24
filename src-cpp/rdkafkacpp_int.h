9afe5cab7735f2b5224341c01a671fa4fb3726fe
David Wieland <d.wieland@cuculus.net>
2016-04-24 20:36:08 +0200

    issue #539 - added symbol test, added c++ impl

--- src-cpp/rdkafkacpp_int.h
+++ src-cpp/rdkafkacpp_int.h
@@ -581,7 +581,10 @@ public:
 	  std::vector<TopicPartition*> empty;
 	  return assign(empty);
   }
+  ErrorCode redirectPartitionQueue(const TopicPartition *partition,
+                                   Queue *queue);
   Message *consume (int timeout_ms);
+  Message *consume (Queue *queue, int timeout_ms);
   ErrorCode commitSync () {
     return static_cast<ErrorCode>(rd_kafka_commit(rk_, NULL, 0/*sync*/));
   }

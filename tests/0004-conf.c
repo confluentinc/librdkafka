8d96733529f6d9a788d1620760a4bf529cb00057
Magnus Edenhill <magnus@edenhill.se>
2016-09-13 12:50:23 +0200

    Dont test regexp if not available

--- tests/0004-conf.c
+++ tests/0004-conf.c
@@ -118,7 +118,9 @@ int main_0004_conf (int argc, char **argv) {
 		"message.max.bytes", "12345", /* int property */
 		"client.id", "my id", /* string property */
 		"debug", "topic,metadata", /* S2F property */
+#if HAVE_REGEX
 		"topic.blacklist", "__.*", /* #778 */
+#endif
 #if WITH_ZLIB
 		"compression.codec", "gzip", /* S2I property */
 #endif

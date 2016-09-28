aee003a3c2c19c98ce90fb3c2ae6d36f70f77089
Magnus Edenhill <magnus@edenhill.se>
2016-09-29 00:27:24 +0200

    Fix possible hang on consumer_close if EVENT_REBALANCE was enabled

--- src/rdkafka.c
+++ src/rdkafka.c
@@ -2098,13 +2098,16 @@ int rd_kafka_poll_cb (rd_kafka_t *rk, rd_kafka_op_t *rko,
 		break;
 
         case RD_KAFKA_OP_REBALANCE:
-		// FIXME: what if EVENT_REBALANCE is enabled but rebalance_cb isnt,
-		//  then the application must call assign() to sync state.
+		/* If EVENT_REBALANCE is enabled but rebalance_cb isnt
+		 * we need to perform a dummy assign for the application.
+		 * This might happen during termination with consumer_close() */
 		if (rk->rk_conf.rebalance_cb)
 			rk->rk_conf.rebalance_cb(
 				rk, rko->rko_err,
 				rko->rko_u.rebalance.partitions,
 				rk->rk_conf.opaque);
+		else
+			rd_kafka_assign(rk, NULL);
 		break;
 
         case RD_KAFKA_OP_OFFSET_COMMIT | RD_KAFKA_OP_REPLY:

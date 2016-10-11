91151ed46c5cbb4da545871545a793c0df24c202
Magnus Edenhill <magnus@edenhill.se>
2016-10-11 13:18:01 +0200

    RD_KAFKA_VERSION bump to 0.9.2-pre0

--- src-cpp/rdkafkacpp.h
+++ src-cpp/rdkafkacpp.h
@@ -87,7 +87,7 @@ namespace RdKafka {
  * @remark This value should only be used during compile time,
  *         for runtime checks of version use RdKafka::version()
  */
-#define RD_KAFKA_VERSION  0x000901ff
+#define RD_KAFKA_VERSION  0x00090200
 
 /**
  * @brief Returns the librdkafka version as integer.

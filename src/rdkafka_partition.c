6301fabc2baf181f6e75ab55f4780e4fd882d891
Magnus Edenhill <magnus@edenhill.se>
2016-10-19 17:36:05 +0200

    Force offset-wait -> offset-query state transition when partition leader changes (issue #843)

--- src/rdkafka_partition.c
+++ src/rdkafka_partition.c
@@ -670,6 +670,21 @@ static void rd_kafka_toppar_broker_migrate (rd_kafka_toppar_t *rktp,
         if (had_next_leader)
                 return;
 
+	/* Revert from offset-wait state back to offset-query
+	 * prior to leaving the broker to avoid stalling
+	 * on the new broker waiting for a offset reply from
+	 * this old broker (that might not come and thus need
+	 * to time out..slowly) */
+	if (rktp->rktp_fetch_state == RD_KAFKA_TOPPAR_FETCH_OFFSET_WAIT) {
+		rd_kafka_toppar_set_fetch_state(
+			rktp, RD_KAFKA_TOPPAR_FETCH_OFFSET_QUERY);
+		rd_kafka_timer_start(&rktp->rktp_rkt->rkt_rk->rk_timers,
+				     &rktp->rktp_offset_query_tmr,
+				     500*1000,
+				     rd_kafka_offset_query_tmr_cb,
+				     rktp);
+	}
+
         if (old_rkb) {
                 /* If there is an existing broker for this toppar we let it
                  * first handle its own leave and then trigger the join for
@@ -721,6 +736,16 @@ void rd_kafka_toppar_broker_leave_for_remove (rd_kafka_toppar_t *rktp) {
 			     rktp->rktp_partition, rktp);
 		return;
 	}
+
+
+	/* Revert from offset-wait state back to offset-query
+	 * prior to leaving the broker to avoid stalling
+	 * on the new broker waiting for a offset reply from
+	 * this old broker (that might not come and thus need
+	 * to time out..slowly) */
+	if (rktp->rktp_fetch_state == RD_KAFKA_TOPPAR_FETCH_OFFSET_WAIT)
+		rd_kafka_toppar_set_fetch_state(
+			rktp, RD_KAFKA_TOPPAR_FETCH_OFFSET_QUERY);
 
 	rko = rd_kafka_op_new(RD_KAFKA_OP_PARTITION_LEAVE);
         rko->rko_rktp = rd_kafka_toppar_keep(rktp);
@@ -994,6 +1019,12 @@ static void rd_kafka_toppar_handle_Offset (rd_kafka_t *rk,
         rd_kafka_toppar_t *rktp = rd_kafka_toppar_s2i(s_rktp);
         int64_t Offset;
 	size_t offset_cnt = 1;
+
+	rd_kafka_toppar_lock(rktp);
+	/* Drop reply from previous partition leader */
+	if (rktp->rktp_leader != rkb)
+		err = RD_KAFKA_RESP_ERR__OUTDATED;
+	rd_kafka_toppar_unlock(rktp);
 
         /* Parse and return Offset */
         err = rd_kafka_handle_Offset(rkb->rkb_rk, rkb, err,

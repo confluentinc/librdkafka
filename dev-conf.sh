8afbc071f72e7bfd39254fc3f09a4051a74ebb2a
Magnus Edenhill <magnus@edenhill.se>
2017-01-06 00:13:52 +0100

    dev-conf: use AddressSanitizer

--- dev-conf.sh
+++ dev-conf.sh
@@ -6,5 +6,10 @@ set -e
 ./configure --clean
 #export CFLAGS='-std=c99 -pedantic -Wshadow'
 #export CXXFLAGS='-std=c++98 -pedantic'
-./configure --enable-devel --enable-werror --disable-optimization
-            #--enable-sharedptr-debug #--enable-refcnt-debug
+
+FSAN="-fsanitize=address"
+export CPPFLAGS="$CPPFLAGS $FSAN"
+export LDFLAGS="$LDFLAGS $FSAN"
+./configure --enable-devel --enable-werror
+#--disable-optimization
+#            --enable-sharedptr-debug #--enable-refcnt-debug

db6739b13d58f733bfd92efdb3284b8b5f6f8c27
Magnus Edenhill <magnus@edenhill.se>
2017-04-07 08:49:19 +0200

    Partial d3676c5cdce28 revert: Don't emit TIMED_OUT_QUEUE
    While it might be interesting for librdkafka developers
    to see the difference between in-queue and in-flight timeouts
    this change also changed the expected error returns from delivery reports, et.al.,
    and thus was a badly documented API:ish change.
    
    This commit reverts back to the previous functionality with emitting TIMED_OUT
    for in-queue timeouts.
    
    (cherry picked from commit 841e8dd6994afb9ece59df4601d084b8d3897604)

--- src/rdkafka_broker.c
+++ src/rdkafka_broker.c
@@ -550,11 +550,11 @@ static void rd_kafka_broker_timeout_scan (rd_kafka_broker_t *rkb, rd_ts_t now) {
 	/* Requests in retry queue */
 	retry_cnt = rd_kafka_broker_bufq_timeout_scan(
 		rkb, 0, &rkb->rkb_retrybufs, NULL,
-		RD_KAFKA_RESP_ERR__TIMED_OUT_QUEUE, now);
+		RD_KAFKA_RESP_ERR__TIMED_OUT, now);
 	/* Requests in local queue not sent yet. */
 	q_cnt = rd_kafka_broker_bufq_timeout_scan(
 		rkb, 0, &rkb->rkb_outbufs, &req_cnt,
-		RD_KAFKA_RESP_ERR__TIMED_OUT_QUEUE, now);
+		RD_KAFKA_RESP_ERR__TIMED_OUT, now);
 
 	if (req_cnt + retry_cnt + q_cnt > 0) {
 		rd_rkb_dbg(rkb, MSG|RD_KAFKA_DBG_BROKER,

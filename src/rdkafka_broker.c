386516a9393238a5ddf003950da8d18c4afbe8de
Magnus Edenhill <magnus@edenhill.se>
2017-04-20 14:23:00 +0200

    ERR_remove_thread_state OpenSSL version checking

--- src/rdkafka_broker.c
+++ src/rdkafka_broker.c
@@ -4795,7 +4795,12 @@ static int rd_kafka_broker_thread_main (void *arg) {
 
 #if WITH_SSL
         /* Remove OpenSSL per-thread error state to avoid memory leaks */
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
+        /*(OpenSSL libraries handle thread init and deinit)
+         * https://github.com/openssl/openssl/pull/1048 */
+#elif OPENSSL_VERSION_NUMBER >= 0x10000000L
         ERR_remove_thread_state(NULL);
+#endif
 #endif
 
 	rd_atomic32_sub(&rd_kafka_thread_cnt_curr, 1);

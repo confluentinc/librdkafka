fde03dc2961850fce5620c0d86668137399f7a83
Bar Rotstein <rotsteinbar@gmail.com>
2017-10-20 11:33:03 +0300

    use endian neutral murmur2hash

--- src/rdmurmur2.h
+++ src/rdmurmur2.h
@@ -13,7 +13,7 @@ typedef uint32_t rd_murmur2_t;
 #define MURMUR2_SEED 0x9747b28c
 
 static RD_INLINE rd_murmur2_t rd_murmur2 (const char *data, size_t data_len) {
-    return MurmurHash2(data, data_len, MURMUR2_SEED);
+    return MurmurHashNeutral2(data, data_len, MURMUR2_SEED);
 }
 
 #ifdef __cplusplus

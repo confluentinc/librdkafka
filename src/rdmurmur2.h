a2e7bf6c4ae5658a49fc463af2ad6dafcceba807
Bar Rotstein <rotsteinbar@gmail.com>
2017-10-21 14:34:44 +0300

    added unitests for murmur2 hash

--- src/rdmurmur2.h
+++ src/rdmurmur2.h
@@ -6,15 +6,22 @@ extern "C" {
 #endif
 
 #include "MurMurHash2.h"
-#include "rd.h"
 
 typedef uint32_t rd_murmur2_t;
 
 #define MURMUR2_SEED 0x9747b28c
 
-static RD_INLINE rd_murmur2_t rd_murmur2 (const char *data, size_t data_len) {
-    return MurmurHashNeutral2(data, data_len, MURMUR2_SEED);
+#define INT32_TO_UINT32 0x7FFFFFFF
+
+static RD_INLINE rd_murmur2_t rd_murmur2(const char *data, size_t data_len) {
+  /*
+   * last bit is set to 0 because the java implementation uses int_32
+   * and then sets to positive number flipping last bit to 1
+   */
+  return MurmurHashNeutral2(data, data_len, MURMUR2_SEED) & INT32_TO_UINT32;
 }
+
+int unittest_murmurhashneutral2(void);
 
 #ifdef __cplusplus
 }

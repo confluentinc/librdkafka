2e375d62e0fd0c230b58baf373bdce32770787be
Magnus Edenhill <magnus@edenhill.se>
2018-01-02 08:32:25 +0100

    Refactor murmur2 code and add more unit tests (unaligned access)

--- src/rdmurmur2.h
+++ src/rdmurmur2.h
@@ -1,22 +1,7 @@
 #ifndef __RDMURMUR2___H__
 #define __RDMURMUR2___H__
 
-#include "MurMurHash2.h"
-
-typedef uint32_t rd_murmur2_t;
-
-#define MURMUR2_SEED 0x9747b28c
-
-#define INT32_TO_UINT32 0x7FFFFFFF
-
-static RD_INLINE rd_murmur2_t rd_murmur2(const char *data, size_t data_len) {
-  /*
-   * last bit is set to 0 because the java implementation uses int_32
-   * and then sets to positive number flipping last bit to 1
-   */
-  return MurmurHashNeutral2(data, data_len, MURMUR2_SEED) & INT32_TO_UINT32;
-}
-
-int unittest_murmurhashneutral2(void);
+uint32_t rd_murmur2 (const void *key, size_t len);
+int unittest_murmur2 (void);
 
 #endif // __RDMURMUR2___H__

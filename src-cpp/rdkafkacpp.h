5949521b7d0bb9982e68140eed8e7e03d6a8d6e0
Magnus Edenhill <magnus@edenhill.se>
2018-04-27 09:26:20 +0200

    Bump version to v0.11.5-PRE0 to support conditional compilation in downstream projects

--- src-cpp/rdkafkacpp.h
+++ src-cpp/rdkafkacpp.h
@@ -99,7 +99,7 @@ namespace RdKafka {
  * @remark This value should only be used during compile time,
  *         for runtime checks of version use RdKafka::version()
  */
-#define RD_KAFKA_VERSION  0x000b04ff
+#define RD_KAFKA_VERSION  0x000b0500
 
 /**
  * @brief Returns the librdkafka version as integer.

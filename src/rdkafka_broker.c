961c60e1d283af696bbec57f2a172d95485bd6d7
Magnus Edenhill <magnus@edenhill.se>
2018-08-07 12:34:26 +0200

    Remove unnecessary 100ms sleep when broker goes from UP -> DOWN (#1895)

--- src/rdkafka_broker.c
+++ src/rdkafka_broker.c
@@ -3560,10 +3560,6 @@ static int rd_kafka_broker_thread_main (void *arg) {
                                 rd_kafka_broker_lock(rkb);
 				rd_kafka_broker_set_state(rkb, RD_KAFKA_BROKER_STATE_UP);
                                 rd_kafka_broker_unlock(rkb);
-			} else {
-				/* Connection torn down, sleep a short while to
-				 * avoid busy-looping on protocol errors */
-				rd_usleep(100*1000/*100ms*/, &rk->rk_terminate);
 			}
 			break;
 		}

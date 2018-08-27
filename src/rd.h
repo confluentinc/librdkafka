88bdd8a2004658bbff48256dafc73373440b4331
Magnus Edenhill <magnus@edenhill.se>
2018-08-27 10:50:43 +0200

    Use atomics for refcounts on all platforms with atomics support (#1873)

--- src/rd.h
+++ src/rd.h
@@ -230,8 +230,8 @@ static RD_INLINE RD_UNUSED void *rd_memdup (const void *src, size_t size) {
 /**
  * Generic refcnt interface
  */
-#ifndef _MSC_VER
-/* Mutexes (critical sections) are slow, even when uncontended, on Windows */
+
+#if !HAVE_ATOMICS_32
 #define RD_REFCNT_USE_LOCKS 1
 #endif
 

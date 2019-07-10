22ca4ae93e2dfb684b66957787bc719652022c5f
Christopher Broadbent <chris-broadbent@zencontrol.com>
2019-07-11 08:39:25 +1000

    Fix UB left shift in zigzag encoding
    Neither latest gcc/clang seem to have codegen differences based
    on this change, and are unlikely ever to since left shift of
    negative isn't UB in c++20. It's still worth fixing to get
    less print outs when running with fsanitize=undefined

--- src/rdvarint.h
+++ src/rdvarint.h
@@ -66,7 +66,7 @@ size_t rd_uvarint_enc_u64 (char *dst, size_t dstsize, uint64_t num) {
  */
 static RD_INLINE RD_UNUSED
 size_t rd_uvarint_enc_i64 (char *dst, size_t dstsize, int64_t num) {
-        return rd_uvarint_enc_u64(dst, dstsize, (num << 1) ^ (num >> 63));
+        return rd_uvarint_enc_u64(dst, dstsize, ((uint64_t)num << 1) ^ (num >> 63));
 }
 
 

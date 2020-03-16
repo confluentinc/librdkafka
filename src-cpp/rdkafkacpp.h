2d3b16c3cc246233120c72141a3aa9b5fc13d8d0
Magnus Edenhill <magnus@edenhill.se>
2020-03-16 10:26:18 +0100

    C++: Fix missing quote in API docs

--- src-cpp/rdkafkacpp.h
+++ src-cpp/rdkafkacpp.h
@@ -910,7 +910,7 @@ public:
    *           consumer->unassign();
    *
    *         } else {
-   *           std::cerr << "Rebalancing error: <<
+   *           std::cerr << "Rebalancing error: " <<
    *                        RdKafka::err2str(err) << std::endl;
    *           consumer->unassign();
    *         }

1007ab08a924f5beb17d4fa145e0c8e5d15f800e
Magnus Edenhill <magnus@edenhill.se>
2020-05-08 10:31:47 +0200

    Test 0069: avoid topic creation race

--- tests/testshared.h
+++ tests/testshared.h
@@ -73,6 +73,8 @@ void test_delete_topic (rd_kafka_t *use_rk, const char *topicname);
 void test_create_topic (rd_kafka_t *use_rk, const char *topicname,
                         int partition_cnt, int replication_factor);
 
+void test_wait_topic_exists (rd_kafka_t *rk, const char *topic, int tmout);
+
 void test_kafka_cmd (const char *fmt, ...);
 
 uint64_t

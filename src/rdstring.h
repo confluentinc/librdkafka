280598889493157858750b42f3114e5f8bfc7817
Magnus Edenhill <magnus@edenhill.se>
2020-05-14 21:57:30 +0200

    Add simple generic and strictly typed hash maps

--- src/rdstring.h
+++ src/rdstring.h
@@ -75,6 +75,9 @@ void *rd_strtup_list_copy (const void *elem, void *opaque);
 char *rd_flags2str (char *dst, size_t size,
                     const char **desc, int flags);
 
+unsigned int rd_string_hash (const char *str, ssize_t len);
+
+int rd_strcmp (const char *a, const char *b);
 
 /** @returns "true" if EXPR is true, else "false" */
 #define RD_STR_ToF(EXPR) ((EXPR) ? "true" : "false")

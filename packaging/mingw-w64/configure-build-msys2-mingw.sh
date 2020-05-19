a7bbcaee39201e4e87e60623a2d4c68541dec800
neptoess <neptoess@gmail.com>
2020-05-19 12:28:30 -0400

    Get rid of extra spaces

--- packaging/mingw-w64/configure-build-msys2-mingw.sh
+++ packaging/mingw-w64/configure-build-msys2-mingw.sh
@@ -4,17 +4,17 @@ set -e
 
 $mingw64 cmake \
       -G "MinGW Makefiles" \
-      -D CMAKE_INSTALL_PREFIX="$PWD/dest/" \
-      -D WITHOUT_WIN32_CONFIG=ON  \
-      -D RDKAFKA_BUILD_EXAMPLES=ON \
-      -D RDKAFKA_BUILD_TESTS=ON \
-      -D WITH_LIBDL=OFF \
-      -D WITH_PLUGINS=OFF \
-      -D WITH_SASL=ON \
-      -D WITH_SSL=ON \
-      -D WITH_ZLIB=OFF \
-      -D RDKAFKA_BUILD_STATIC=OFF \
-      -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE .
+      -DCMAKE_INSTALL_PREFIX="$PWD/dest/" \
+      -DWITHOUT_WIN32_CONFIG=ON  \
+      -DRDKAFKA_BUILD_EXAMPLES=ON \
+      -DRDKAFKA_BUILD_TESTS=ON \
+      -DWITH_LIBDL=OFF \
+      -DWITH_PLUGINS=OFF \
+      -DWITH_SASL=ON \
+      -DWITH_SSL=ON \
+      -DWITH_ZLIB=OFF \
+      -DRDKAFKA_BUILD_STATIC=OFF \
+      -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE .
 
 $mingw64 mingw32-make
 $mingw64 mingw32-make install

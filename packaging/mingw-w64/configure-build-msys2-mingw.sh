d963b7169299f66618f239115734d9d1fda2223c
neptoess <neptoess@gmail.com>
2020-05-19 14:06:10 -0400

    Try calling cmake directly instead of passing to msys shell

--- packaging/mingw-w64/configure-build-msys2-mingw.sh
+++ packaging/mingw-w64/configure-build-msys2-mingw.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-$mingw64 cmake \
-      -G "MinGW Makefiles" \
-      -D CMAKE_INSTALL_PREFIX\="$PWD/dest/" \
-      -D WITHOUT_WIN32_CONFIG\=ON  \
-      -D RDKAFKA_BUILD_EXAMPLES\=ON \
-      -D RDKAFKA_BUILD_TESTS\=ON \
-      -D WITH_LIBDL\=OFF \
-      -D WITH_PLUGINS\=OFF \
-      -D WITH_SASL\=ON \
-      -D WITH_SSL\=ON \
-      -D WITH_ZLIB\=OFF \
-      -D RDKAFKA_BUILD_STATIC\=OFF \
-      -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS\=TRUE .
+cmake \
+    -G "MinGW Makefiles" \
+    -D CMAKE_INSTALL_PREFIX="$PWD/dest/" \
+    -D WITHOUT_WIN32_CONFIG=ON  \
+    -D RDKAFKA_BUILD_EXAMPLES=ON \
+    -D RDKAFKA_BUILD_TESTS=ON \
+    -D WITH_LIBDL=OFF \
+    -D WITH_PLUGINS=OFF \
+    -D WITH_SASL=ON \
+    -D WITH_SSL=ON \
+    -D WITH_ZLIB=OFF \
+    -D RDKAFKA_BUILD_STATIC=OFF \
+    -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE .
 
 $mingw64 mingw32-make
 $mingw64 mingw32-make install

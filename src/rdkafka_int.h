6c8a3a99d05d3a0f6cc0ae9b1b44bf075fddde37
Magnus Edenhill <magnus@edenhill.se>
2020-06-09 11:20:51 +0200

    Added 'assignor' debug context for debugging partition assignors

--- src/rdkafka_int.h
+++ src/rdkafka_int.h
@@ -812,6 +812,7 @@ const char *rd_kafka_purge_flags2str (int flags);
 #define RD_KAFKA_DBG_ADMIN          0x4000
 #define RD_KAFKA_DBG_EOS            0x8000
 #define RD_KAFKA_DBG_MOCK           0x10000
+#define RD_KAFKA_DBG_ASSIGNOR       0x20000
 #define RD_KAFKA_DBG_ALL            0xfffff
 #define RD_KAFKA_DBG_NONE           0x0
 

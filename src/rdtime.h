b7550c7c6b9256ccc0a2a3ae31115266632e3e04
Yagna Srinath Reddy Battula <yagnasrinath@users.noreply.github.com>
2020-08-31 23:55:09 -0700

    Not using timespec_get on android api level < 29 (@yagnasrinath, #3048)
    * Not using timespec_get on android api level < 29
    
    * corrected the macro variable

--- src/rdtime.h
+++ src/rdtime.h
@@ -79,7 +79,7 @@ BOOL rd_ut_QueryPerformanceCounter(_Out_ LARGE_INTEGER * lpPerformanceCount);
  */
 static RD_INLINE rd_ts_t rd_clock (void) RD_UNUSED;
 static RD_INLINE rd_ts_t rd_clock (void) {
-#ifdef __APPLE__
+#if defined(__APPLE__) || (defined(__ANDROID__) && __ANDROID_API__ < 29)
 	/* No monotonic clock on Darwin */
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
@@ -195,7 +195,7 @@ static RD_INLINE void rd_timeout_init_timespec_us (struct timespec *tspec,
                 tspec->tv_sec = timeout_us;
                 tspec->tv_nsec = 0;
         } else {
-#ifdef __APPLE__
+#if defined(__APPLE__) || (defined(__ANDROID__) && __ANDROID_API__ < 29)
                 struct timeval tv;
                 gettimeofday(&tv, NULL);
                 TIMEVAL_TO_TIMESPEC(&tv, tspec);
@@ -226,7 +226,7 @@ static RD_INLINE void rd_timeout_init_timespec (struct timespec *tspec,
                 tspec->tv_sec = timeout_ms;
                 tspec->tv_nsec = 0;
         } else {
-#ifdef __APPLE__
+#if defined(__APPLE__) || (defined(__ANDROID__) && __ANDROID_API__ < 29)
                 struct timeval tv;
                 gettimeofday(&tv, NULL);
                 TIMEVAL_TO_TIMESPEC(&tv, tspec);

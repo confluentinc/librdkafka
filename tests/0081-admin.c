91822f8a392fe9de1e4f3761254be6b5bdee92a7
Magnus Edenhill <magnus@edenhill.se>
2020-11-06 10:13:39 +0100

    Test 0081: AK 2.7.0 returns proper error code for AlterConfigs on unknown topic

--- tests/0081-admin.c
+++ tests/0081-admin.c
@@ -788,7 +788,10 @@ static void do_test_AlterConfigs (rd_kafka_t *rk, rd_kafka_queue_t *rkqu) {
                                                  "12345");
         TEST_ASSERT(!err, "%s", rd_kafka_err2str(err));
 
-        exp_err[ci] = RD_KAFKA_RESP_ERR_UNKNOWN;
+        if (test_broker_version >= TEST_BRKVER(2, 7, 0, 0))
+                exp_err[ci] = RD_KAFKA_RESP_ERR_UNKNOWN_TOPIC_OR_PART;
+        else
+                exp_err[ci] = RD_KAFKA_RESP_ERR_UNKNOWN;
         ci++;
 
 

b4399accf51653b0bb0cbd6ea873c1cd04a15e0c
Magnus Edenhill <magnus@edenhill.se>
2021-01-15 13:29:54 +0100

    Build pypa/manylinux2010 to replace debian9 build in nuget librdkafka.redist

--- packaging/nuget/packaging.py
+++ packaging/nuget/packaging.py
@@ -336,8 +336,8 @@ class NugetPackage (Package):
 
             # Travis OSX build
             [{'arch': 'x64', 'plat': 'osx', 'fname_glob': 'librdkafka-clang.tar.gz'}, './lib/librdkafka.dylib', 'runtimes/osx-x64/native/librdkafka.dylib'],
-            # Travis Debian 9 / Ubuntu 16.04 build
-            [{'arch': 'x64', 'plat': 'linux', 'fname_glob': 'librdkafka-debian9.tgz'}, './lib/librdkafka.so.1', 'runtimes/linux-x64/native/debian9-librdkafka.so'],
+            # Travis Manylinux build
+            [{'arch': 'x64', 'plat': 'linux', 'fname_glob': 'librdkafka-manylinux*x86_64.tgz'}, './lib/librdkafka.so.1', 'runtimes/linux-x64/native/centos6-librdkafka.so'],
             # Travis Ubuntu 14.04 build
             [{'arch': 'x64', 'plat': 'linux', 'fname_glob': 'librdkafka-gcc.tar.gz'}, './lib/librdkafka.so.1', 'runtimes/linux-x64/native/librdkafka.so'],
             # Travis CentOS 7 RPM build
@@ -442,7 +442,7 @@ class NugetPackage (Package):
             "build/native/lib/win/x86/win-x86-Release/v120/librdkafka.lib",
             "build/native/lib/win/x86/win-x86-Release/v120/librdkafkacpp.lib",
             "runtimes/linux-x64/native/centos7-librdkafka.so",
-            "runtimes/linux-x64/native/debian9-librdkafka.so",
+            "runtimes/linux-x64/native/centos6-librdkafka.so",
             "runtimes/linux-x64/native/alpine-librdkafka.so",
             "runtimes/linux-x64/native/librdkafka.so",
             "runtimes/linux-arm64/native/librdkafka.so",

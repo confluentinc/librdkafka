f077e493cecde4c1da38c364845bf6ddebab523a
Magnus Edenhill <magnus@edenhill.se>
2022-10-18 18:30:09 +0200

    Skip some tests that require java tools when SSL is configured

--- tests/0077-compaction.c
+++ tests/0077-compaction.c
@@ -328,6 +328,11 @@ int main_0077_compaction(int argc, char **argv) {
         if (!test_can_create_topics(1))
                 return 0;
 
+        if (test_needs_auth()) {
+                TEST_SKIP("Test cluster requires authentication/SSL\n");
+                return 0;
+        }
+
         do_test_compaction(10, NULL);
 
         if (test_quick) {

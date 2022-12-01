61955a9ed14b1a771322beb2a33a418d632567a1
Roxane Fruytier <roxane.fruytier@datadoghq.com>
2022-12-01 10:04:49 -0500

    Add test

--- tests/test.c
+++ tests/test.c
@@ -246,6 +246,7 @@ _TEST_DECL(0135_sasl_credentials);
 _TEST_DECL(0136_resolve_cb);
 _TEST_DECL(0137_barrier_batch_consume);
 _TEST_DECL(0138_admin_mock);
+_TEST_DECL(0139_cooperative_commit_rebalance);
 
 /* Manual tests */
 _TEST_DECL(8000_idle);
@@ -490,6 +491,7 @@ struct test tests[] = {
     _TEST(0136_resolve_cb, TEST_F_LOCAL),
     _TEST(0137_barrier_batch_consume, 0),
     _TEST(0138_admin_mock, TEST_F_LOCAL, TEST_BRKVER(2, 4, 0, 0)),
+    _TEST(0139_cooperative_commit_rebalance, 0),
 
     /* Manual tests */
     _TEST(8000_idle, TEST_F_MANUAL),

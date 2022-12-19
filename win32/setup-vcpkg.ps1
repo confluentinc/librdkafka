d6a3b51ac86eb6f493fd6b7b9923c72d8ed80e55
Magnus Edenhill <magnus@edenhill.se>
2022-12-19 10:57:08 +0100

    Update vcpkg repo so that baseline commit is available

--- win32/setup-vcpkg.ps1
+++ win32/setup-vcpkg.ps1
@@ -2,6 +2,11 @@
 
 if (!(Test-Path -Path vcpkg/.git)) {
     git clone https://github.com/Microsoft/vcpkg.git
+} else {
+    echo "Updating vcpkg git repo"
+    cd vcpkg
+    git pull
+    cd ..
 }
 
 .\vcpkg\bootstrap-vcpkg.bat

e89068e062d10e4150c31adb7287ab1288999b4b
Pranav Rathi <pranav.rth@gmail.com>
2023-03-30 18:01:25 +0530

    Fixing vcpkg integrate install issue

--- win32/setup-vcpkg.ps1
+++ win32/setup-vcpkg.ps1
@@ -2,6 +2,9 @@
 
 if (!(Test-Path -Path vcpkg/.git)) {
     git clone https://github.com/Microsoft/vcpkg.git
+    cd vcpkg
+    git checkout 328bd79eb8340b8958f567aaf5f8ffb81056cd36
+    cd ..
 } else {
     echo "Updating vcpkg git repo"
     cd vcpkg

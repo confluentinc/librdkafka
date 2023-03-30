3d684f5f16fb69f2fd9bef11f36a8ea28c03adab
Pranav Rathi <pranav.rth@gmail.com>
2023-03-30 18:44:29 +0530

    Ignore permission denied

--- win32/install-coapp.ps1
+++ win32/install-coapp.ps1
@@ -9,8 +9,17 @@ Start-Process -FilePath msiexec -ArgumentList /i, $msiPath, /quiet -Wait
 # Make the tools available for later PS scripts to use.
 $env:PSModulePath = $env:PSModulePath + ';C:\\Program Files (x86)\\Outercurve Foundation\\Modules'
 
-Get-module -ListAvailable
+# Get-module -ListAvailable
 
-Get-ChildItem C:\\ -include *CoApp* -recurse
+Get-ChildItem C:\\ -include *CoApp* -recurse -ErrorAction Continue 2>&1|%{
+  if($_ -is [System.Management.Automation.ErrorRecord]){
+    # if this is an error, print the exception message directly to the screen
+    Write-Host $_.Exception.Message -ForegroundColor Red
+  }
+  else {
+    # otherwise, just output the input object as-is
+    $_
+  }
+}
 
 Import-Module CoApp

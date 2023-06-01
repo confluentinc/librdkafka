9df9b4b73ad39e6a3038df16d14fffa2d155db04
Jeroen Demeyer <jeroen.demeyer@be-mobile.com>
2023-06-01 12:18:42 +0200

    Typo in KIP-464 error message

--- src/rdkafka_request.c
+++ src/rdkafka_request.c
@@ -4894,7 +4894,7 @@ rd_kafka_CreateTopicsRequest(rd_kafka_broker_t *rkb,
                                 rd_snprintf(errstr, errstr_size,
                                             "Default partition count (KIP-464) "
                                             "not supported by broker, "
-                                            "requires broker version <= 2.4.0");
+                                            "requires broker version >= 2.4.0");
                                 rd_kafka_replyq_destroy(&replyq);
                                 rd_kafka_buf_destroy(rkbuf);
                                 return RD_KAFKA_RESP_ERR__UNSUPPORTED_FEATURE;
@@ -4906,7 +4906,7 @@ rd_kafka_CreateTopicsRequest(rd_kafka_broker_t *rkb,
                                             "Default replication factor "
                                             "(KIP-464) "
                                             "not supported by broker, "
-                                            "requires broker version <= 2.4.0");
+                                            "requires broker version >= 2.4.0");
                                 rd_kafka_replyq_destroy(&replyq);
                                 rd_kafka_buf_destroy(rkbuf);
                                 return RD_KAFKA_RESP_ERR__UNSUPPORTED_FEATURE;

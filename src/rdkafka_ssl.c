887b51cc0cbf6a3466535941d38be37daa790cb0
Jan Kevin Dick <Jan.Dick@de.bosch.com>
2023-08-25 09:48:50 +0200

    Added missing Pragma for the Debug Version of the libssl.lib

--- src/rdkafka_ssl.c
+++ src/rdkafka_ssl.c
@@ -42,6 +42,7 @@
 #pragma comment(lib, "crypt32.lib")
 #ifdef _DEBUG
 #pragma comment(lib, "libcryptod.lib")
+#pragma comment(lib, "libssld.lib")
 #else
 #pragma comment(lib, "libcrypto.lib")
 #pragma comment(lib, "libssl.lib")

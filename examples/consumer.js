c4057d3223c449a7d42aa11f844c0e008173b78a
Milind L <miluthra@confluent.io>
2024-01-09 12:20:33 +0530

    Add simpler consume() function besides run() and update example

--- examples/consumer.js
+++ examples/consumer.js
@@ -1,83 +1,33 @@
 const { Kafka, ErrorCodes } = require('../').KafkaJS
 
+
 async function consumerStart() {
+  let consumer;
   let stopped = false;
 
-  // Initialization
-  const consumer = new Kafka().consumer({
-    'bootstrap.servers': 'localhost:9092',
-    'group.id': 'test-group2',
-    'auto.offset.reset': 'earliest',
-    'rebalance_cb': (err, assignment) => {
-        switch (err.code) {
-            case ErrorCodes.ERR__ASSIGN_PARTITIONS:
-                console.log(`Assigned partitions ${JSON.stringify(assignment)}`);
-                pauseResumeLoop();
-                break;
-            case ErrorCodes.ERR__REVOKE_PARTITIONS:
-                console.log(`Revoked partitions ${JSON.stringify(assignment)}`);
-                break;
-            default:
-                console.error(err);
-        }
-    },
-  });
-
-  await consumer.connect();
-  console.log("Connected successfully");
-
-  await consumer.subscribe({ topics: [ "topic2" ] });
-
-  // Consume example with seek.
-  let batch = 0;
-  consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        topic,
-        partition,
-        offset: message.offset,
-        key: message.key?.toString(),
-        value: message.value.toString(),
-      })
-
-      // Seek example: seek to beginning every 100 messages
-      if (++batch % 100 == 0) {
-        await consumer.seek({
-          topic,
-          partition,
-          offset: -2 /* Offset beginning is -2 */
-        });
-        batch = 0;
-      }
-    },
-  });
-
-  // Pause/Resume example
+  // Pause/Resume example, pause and resume alternately every 2 seconds.
+  let pauseResumeLoopStarted = false;
   const pauseResumeLoop = async () => {
     let paused = false;
-    let ticks = 0;
+    pauseResumeLoopStarted = true;
     while (!stopped) {
-      await new Promise((resolve) => setTimeout(resolve, 100));
+      await new Promise((resolve) => setTimeout(resolve, 2000));
       if (stopped)
         break;
 
-      ticks++;
-      if (ticks == 200) {
-        ticks = 0;
-        const assignment = consumer.assignment();
-        if (paused) {
-          console.log(`Resuming partitions ${JSON.stringify(assignment)}`)
-          consumer.resume(assignment);
-        } else {
-          console.log(`Pausing partitions ${JSON.stringify(assignment)}`);
-          consumer.pause(assignment);
-        }
-        paused = !paused;
+      const assignment = consumer.assignment();
+      if (paused) {
+        console.log(`Resuming partitions ${JSON.stringify(assignment)}`)
+        consumer.resume(assignment);
+      } else {
+        console.log(`Pausing partitions ${JSON.stringify(assignment)}`);
+        consumer.pause(assignment);
       }
+      paused = !paused;
     }
-  }
+  };
 
-  // Disconnect example
+  // Set up signals for a graceful shutdown.
   const disconnect = () => {
     process.off('SIGINT', disconnect);
     process.off('SIGTERM', disconnect);
@@ -92,6 +42,49 @@ async function consumerStart() {
   }
   process.on('SIGINT', disconnect);
   process.on('SIGTERM', disconnect);
+
+
+
+  // Initialization
+  consumer = new Kafka().consumer({
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'test-group3',
+    'auto.offset.reset': 'earliest',
+    'enable.partition.eof': 'true',
+    'rebalance_cb': (err, assignment) => {
+      switch (err.code) {
+        case ErrorCodes.ERR__ASSIGN_PARTITIONS:
+          console.log(`Assigned partitions ${JSON.stringify(assignment)}`);
+          if (!pauseResumeLoopStarted) // Start the pause/resume loop for the example.
+            pauseResumeLoop();
+          break;
+        case ErrorCodes.ERR__REVOKE_PARTITIONS:
+          console.log(`Revoked partitions ${JSON.stringify(assignment)}`);
+          break;
+        default:
+          console.error(err);
+      }
+    },
+  });
+
+  await consumer.connect();
+  console.log("Connected successfully");
+  await consumer.subscribe({ topics: ["test-topic"] });
+
+  // Start the consumer.
+  while (!stopped) {
+    const message = await consumer.consume(1000);
+    if (!message) {
+      continue;
+    }
+    console.log({
+      topic: message.topic,
+      partition: message.partition,
+      offset: message.offset,
+      key: message.key?.toString(),
+      value: message.value.toString(),
+    });
+  }
 }
 
 consumerStart();

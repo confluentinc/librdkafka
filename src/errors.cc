1777f4da2d7c82615b0823669867ec8f008174f2
Milind L <miluthra@confluent.io>
2024-03-15 13:54:56 +0530

    Update copyright headers

--- src/errors.cc
+++ src/errors.cc
@@ -2,6 +2,7 @@
  * confluent-kafka-javascript - Node.js wrapper  for RdKafka C/C++ library
  *
  * Copyright (c) 2016-2023 Blizzard Entertainment
+ *           (c) 2024 Confluent, Inc.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.

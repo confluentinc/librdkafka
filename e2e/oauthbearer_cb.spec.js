127567f6f336ed610ed3c084c85476714dcdd85b
Milind L <miluthra@confluent.io>
2024-04-05 13:28:17 +0530

    Allow token refresh cb to be an async function

--- e2e/oauthbearer_cb.spec.js
+++ e2e/oauthbearer_cb.spec.js
@@ -14,69 +14,88 @@ var eventListener = require('./listener');
 
 var kafkaBrokerList = process.env.KAFKA_HOST || 'localhost:9092';
 
-describe('Client with oauthbearer_cb', function () {
-  const oauthbearer_config = 'key=value';
-  let oauthbearer_cb_called = 0;
-  let oauthbearer_cb = function (config, cb) {
-    console.log("Called oauthbearer_cb with given config: " + config);
-    t.equal(config, oauthbearer_config);
-    oauthbearer_cb_called++;
+const oauthbearer_config = 'key=value';
+let oauthbearer_cb_called = 0;
 
-    // The broker is not expected to be configured for oauthbearer authentication.
-    // We just want to make sure that token refresh callback is triggered.
-    cb(new Error('oauthbearer_cb error'), null);
-  };
+let oauthbearer_cb_callback = function (config, cb) {
+  console.log("Called oauthbearer_cb with given config: " + config);
+  t.equal(config, oauthbearer_config);
+  oauthbearer_cb_called++;
 
-  const commonConfig = {
-    'metadata.broker.list': kafkaBrokerList,
-    'debug': 'all',
-    'security.protocol': 'SASL_PLAINTEXT',
-    'sasl.mechanisms': 'OAUTHBEARER',
-    'oauthbearer_token_refresh_cb': oauthbearer_cb,
-    'sasl.oauthbearer.config': oauthbearer_config,
-  }
+  // The broker is not expected to be configured for oauthbearer authentication.
+  // We just want to make sure that token refresh callback is triggered.
+  cb(new Error('oauthbearer_cb error'), null);
+};
 
-  const checkClient = function (client, done) {
-    eventListener(client);
+let oauthbearer_cb_async = async function (config) {
+  console.log("Called oauthbearer_cb with given config: " + config);
+  t.equal(config, oauthbearer_config);
+  oauthbearer_cb_called++;
 
-    client.on('error', function (e) {
-      t.match(e.message, /oauthbearer_cb error/);
+  // The broker is not expected to be configured for oauthbearer authentication.
+  // We just want to make sure that token refresh callback is triggered.
+  throw new Error('oauthbearer_cb error');
+};
+
+for (const oauthbearer_cb of [oauthbearer_cb_async, oauthbearer_cb_callback]) {
+  describe('Client with ' + (oauthbearer_cb.name), function () {
+
+    const commonConfig = {
+      'metadata.broker.list': kafkaBrokerList,
+      'debug': 'all',
+      'security.protocol': 'SASL_PLAINTEXT',
+      'sasl.mechanisms': 'OAUTHBEARER',
+      'oauthbearer_token_refresh_cb': oauthbearer_cb,
+      'sasl.oauthbearer.config': oauthbearer_config,
+    }
+
+    const checkClient = function (client, done, useCb) {
+      eventListener(client);
+
+      client.on('error', function (e) {
+        t.match(e.message, /oauthbearer_cb error/);
+      });
+
+      // The default timeout for the connect is 30s, so even if we
+      // call disconnect() midway, the test ends up being at least 30s.
+      client.connect({timeout: 2000});
+
+      // We don't actually expect the connection to succeed, but we want to
+      // make sure that the oauthbearer_cb is called so give it a couple seconds.
+      setTimeout(() => {
+        t.equal(oauthbearer_cb_called >= 1, true);
+        client.disconnect(() => {
+          done();
+        });
+        client = null;
+        if (!useCb) // for admin client, where disconnect is sync.
+          done();
+      }, 2000);
+    }
+
+    beforeEach(function (done) {
+      oauthbearer_cb_called = 0;
+      done();
     });
 
-    client.connect();
+    it('as producer', function (done) {
+      let producer = new Kafka.Producer(commonConfig);
+      checkClient(producer, done, true);
+      producer = null;
+    }).timeout(5000);
 
-    // We don't actually expect the connection to succeed, but we want to
-    // make sure that the oauthbearer_cb is called so give it a couple seconds.
-    setTimeout(() => {
-      client.disconnect();
-      client = null;
-      t.equal(oauthbearer_cb_called >= 1, true);
-      done();
-    }, 2000);
-  }
+    it('as consumer', function (done) {
+      const config = Object.assign({ 'group.id': 'gid' }, commonConfig);
+      let consumer = new Kafka.KafkaConsumer(config);
+      checkClient(consumer, done, true);
+      consumer = null;
+    }).timeout(5000);
 
-  beforeEach(function (done) {
-    oauthbearer_cb_called = 0;
-    done();
+    it('as admin', function (done) {
+      let admin = new Kafka.AdminClient.create(commonConfig);
+      checkClient(admin, done, false);
+      admin = null;
+    }).timeout(5000);
+
   });
-
-  it('as producer', function (done) {
-    let producer = new Kafka.Producer(commonConfig);
-    checkClient(producer, done);
-    producer = null;
-  }).timeout(2500);
-
-  it('as consumer', function (done) {
-    const config = Object.assign({ 'group.id': 'gid' }, commonConfig);
-    let consumer = new Kafka.KafkaConsumer(config);
-    checkClient(consumer, done);
-    consumer = null;
-  }).timeout(2500);
-
-  it('as admin', function (done) {
-    let admin = new Kafka.AdminClient.create(commonConfig);
-    checkClient(admin, done);
-    admin = null;
-  }).timeout(2500);
-
-});
+}

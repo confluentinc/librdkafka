d31ed86ff12f7f828e5b30152765cad194450c3e
Emanuele Sabellico <esabellico@confluent.io>
2024-04-17 13:28:23 +0200

    [KIP-848] Use metadata cache by topic id, fixes and improvements (#4661)
    - rename 'generic' protocol to 'classic'
    - consumer group serve timer to awake the loop earlier
    - compare and find into topic partition list by topic id only
    - fix memory leak when instance creation fails and app_conf is provided
    - fix cases where HB response is received after unsubscription
    - use topic name from current assignment if it's missing from metadata
    - expedite heartbeat simplification and
        fixes to next interval check and to the place where
        number of retries is increased
    - expedite HB after changing state back to INIT
    - use the CONSUMER_F_WAIT_REJOIN to trigger
      the rejoin instead of calling it from max poll interval timer
    - schedule timer for next execution
      reschedule it if expected earlier
      expedite through scheduling cgrp serve timer
    - treat unsupported feature error
       as fatal
    - avoid removing partitions not matched by a new subscription
       immediately, for a possible desynchronization with the coordinator, 
       with said partitions not being consumed anymore

--- tests/test.h
+++ tests/test.h
@@ -859,7 +859,7 @@ rd_kafka_resp_err_t test_delete_all_test_topics(int timeout_ms);
 void test_mock_cluster_destroy(rd_kafka_mock_cluster_t *mcluster);
 rd_kafka_mock_cluster_t *test_mock_cluster_new(int broker_cnt,
                                                const char **bootstraps);
-size_t test_mock_wait_maching_requests(
+size_t test_mock_wait_matching_requests(
     rd_kafka_mock_cluster_t *mcluster,
     size_t num,
     int confidence_interval_ms,

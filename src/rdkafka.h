8b81da72e18e53185fccf4e0a9705aded3d60557
lijunlong <lijunlong@openresty.com>
2024-04-29 20:21:40 +0800

    bugfix: compilation failed: error: type qualifiers ignored on function return type.

--- src/rdkafka.h
+++ src/rdkafka.h
@@ -8255,7 +8255,7 @@ const rd_kafka_TopicPartitionInfo_t **rd_kafka_TopicDescription_partitions(
  * @return The partition id.
  */
 RD_EXPORT
-const int rd_kafka_TopicPartitionInfo_partition(
+int rd_kafka_TopicPartitionInfo_partition(
     const rd_kafka_TopicPartitionInfo_t *partition);
 
 

6f1624ee8d459160a0ba19a56ad4cb4207ae27cc
Emanuele Sabellico <esabellico@confluent.io>
2024-08-05 11:29:17 +0200

    Add forward declaration to fix compilation without ssl (#4794)
    and add build checks with different configurations

--- src/rdkafka_op.h
+++ src/rdkafka_op.h
@@ -39,6 +39,7 @@
 typedef struct rd_kafka_q_s rd_kafka_q_t;
 typedef struct rd_kafka_toppar_s rd_kafka_toppar_t;
 typedef struct rd_kafka_op_s rd_kafka_op_t;
+typedef struct rd_kafka_broker_s rd_kafka_broker_t;
 
 /* One-off reply queue + reply version.
  * All APIs that take a rd_kafka_replyq_t makes a copy of the

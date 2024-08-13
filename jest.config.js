a903b6e7a0e5234c72abfeaa369956264f79db41
Justin Wang <35274216+Claimundefine@users.noreply.github.com>
2024-08-13 10:35:02 -0400

    Preset fix (#6)
    * Fix jest to unblock tests
    
    * Add git history

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/test/**/*.ts', '**/e2e/**/*.ts'],
     transform: {
       '^.+\\.tsx?$': 'ts-jest',
     },
   };
+  

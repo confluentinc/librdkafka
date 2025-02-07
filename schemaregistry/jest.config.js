12e175ad09a06073c6069a7b374e4221ab1eb599
Justin Wang <35274216+Claimundefine@users.noreply.github.com>
2025-02-07 15:22:41 -0500

    Add SR testing to semaphore (#252)
    * Add SR testing to semaphore
    
    * Update sr script
    
    * update package.json
    
    * Run tests in band
    
    * Add npm types
    
    * Change SR to own block
    
    * Fix spacing
    
    * Change semaphore node version
    
    * check typescript version
    
    * Set TS version
    
    * Add SR test retries
    
    * Remove debugging commands
    
    * Shift SR tests to original module
    
    * Add comment on fixing first SR test fail

--- schemaregistry/jest.config.js
+++ schemaregistry/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
     transform: {
       '^.+\\.tsx?$': 'ts-jest',
     },
+    "collectCoverage": true,
+    "coverageReporters": ['json', 'text', 'html'],
+    "coverageDirectory": 'coverage/jest/',
   };

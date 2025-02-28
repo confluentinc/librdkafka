60380b06ae17fd453e60c8d7ffb192902b9a035b
Anchit Jain <anjain@confluent.io>
2025-02-28 13:21:21 +0530

    Added config

--- src/rdkafka_conf.h
+++ src/rdkafka_conf.h
@@ -177,7 +177,7 @@ typedef enum {
 
 /* Increase in steps of 64 as needed.
  * This must be larger than sizeof(rd_kafka_[topic_]conf_t) */
-#define RD_KAFKA_CONF_PROPS_IDX_MAX (64 * 33)
+#define RD_KAFKA_CONF_PROPS_IDX_MAX (64 * 34)
 
 /**
  * @struct rd_kafka_anyconf_t

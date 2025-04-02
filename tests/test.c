5bf87e41e21d6f75df7a12c4393a2d9f4e59883c
Emanuele Sabellico <esabellico@confluent.io>
2025-04-02 19:04:55 +0200

    Fix for a warning about using the variable in input and output

--- tests/test.c
+++ tests/test.c
@@ -1106,19 +1106,23 @@ test_conf_set_log_interceptor(rd_kafka_conf_t *conf,
         rd_kafka_conf_set_log_cb(conf, test_conf_log_interceptor_log_cb);
 
         if (!test_debug || !strstr(test_debug, "all")) {
-                char debug_with_contexts[512];
-                rd_snprintf(debug_with_contexts, sizeof(debug_with_contexts),
-                            "%s", test_debug ? test_debug : "");
+                char debug_with_contexts[256] = {0};
+                size_t i                      = rd_snprintf(debug_with_contexts,
+                                       sizeof(debug_with_contexts), "%s",
+                                       test_debug ? test_debug : "");
                 /* Add all debug contexts and set debug configuration */
-                while (*debug_contexts) {
+                while (
+                    *debug_contexts &&
+                    i + strlen(*debug_contexts) +
+                            (i > 0 ? 2 : 1) /* 1 for the comma + 1 for the \0 */
+                        <= sizeof(debug_with_contexts)) {
                         if (!strstr(debug_with_contexts, *debug_contexts)) {
-                                rd_snprintf(debug_with_contexts,
-                                            sizeof(debug_with_contexts),
-                                            "%.*s%s%s",
-                                            (int)strlen(debug_with_contexts),
-                                            debug_with_contexts,
-                                            debug_with_contexts[0] ? "," : "",
-                                            *debug_contexts);
+                                if (i > 0)
+                                        debug_with_contexts[i++] = ',';
+                                i +=
+                                    rd_snprintf(&debug_with_contexts[i],
+                                                sizeof(debug_with_contexts) - i,
+                                                "%s", *debug_contexts);
                         }
                         debug_contexts++;
                 }

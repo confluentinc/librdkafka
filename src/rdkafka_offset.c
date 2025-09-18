13a2bbae88d8be073986f974db1b853cfc91ae8f
Emanuele Sabellico <esabellico@confluent.io>
2025-09-18 09:38:36 +0200

    [KIP-320] Validate assigned partitions before starting to consume from them (#4931)
    * Fetched committed offsets should be validated
    before starting to consume from it.
    Failing test and mock handler implementation
    for returning the committed offset leader epoch
    instead of current leader epoch.
    
    * Validate the offsets before starting to fetch assigned partitions
    
    * Add more test cases for partition assignment
    offset validation
    
    * Fix for test 0139 subtest `do_test_store_offset_without_leader_epoch` . When fetching an offset it returns the leader epoch used when committing, not the current
    leader epoch.
    Given the mock cluster fix the test needs to be changed.
    
    * Fix test `0139` subtest `do_test_list_offsets_leader_change`:
    use cloned partition list for listing offsets, to avoid the fake leader epoch is then used for validation when assigning.
    
    Fix ListOffsets mock handler for logging the correct returned leader epoch.
    
    * Changelog entry
    
    * Reduce number of tests in quick mode
    
    * Add a new fetch state when finishing validating and starting to seek after a truncation,
    to avoid a second repeated validation and possibly duplicated messages.
    
    * Increase single test timeout
    
    * Fix to leave the group in `rd_kafka_cgrp_incr_unassign_done` if terminate was requested, as done in `rd_kafka_cgrp_unassign_done` and `rd_kafka_cgrp_consumer_incr_unassign_done`
    
    * Mock cluster, set the group as empty when last member leaves
    instead of triggering a rebalance
    
    * Test 0139 with mock cluster marked as local.
    Doesn't delete topic if tests are local only as it's
    possible there's no cluster to connect to
    and it speeds up completing the test
    
    * Resume the partition before fetch start or before validation

--- src/rdkafka_offset.c
+++ src/rdkafka_offset.c
@@ -1056,6 +1056,8 @@ static void rd_kafka_toppar_handle_OffsetForLeaderEpoch(rd_kafka_t *rk,
                             end_offset, end_offset_leader_epoch);
 
                 } else {
+                        rd_kafka_toppar_set_fetch_state(
+                            rktp, RD_KAFKA_TOPPAR_FETCH_VALIDATE_SEEK);
                         rd_kafka_toppar_unlock(rktp);
 
                         /* Seek to the updated end offset */

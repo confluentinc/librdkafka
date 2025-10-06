a536e2f6eace2f4e3fc8c531c95dab4112d1f12a
Milind L <miluthra@confluent.io>
2025-10-06 04:59:06 +0000

    Implement polling logic to work with normal fetch
    * Add SHARE_FETCH_FANOUT op to send fetch request to all the brokers
    * Add SHARE_FETCH op and related fields which is sent to each broker
    * select broker to fetch from as only 1 broker is allowed to fetch at a time
    * retry skelton for share fetch replies from brokers
    * add max.poll.records config
    * start returning messages to user from poll from normal fetch
    * Update consumer example to work as share consumer

--- src/rdkafka_broker.c
+++ src/rdkafka_broker.c
@@ -3550,6 +3550,28 @@ rd_kafka_broker_op_serve(rd_kafka_broker_t *rkb, rd_kafka_op_t *rko) {
                 wakeup = rd_true;
                 break;
 
+        case RD_KAFKA_OP_SHARE_FETCH:
+                rd_rkb_dbg(rkb, CGRP, "SHAREFETCH",
+                           "Received SHARE_FETCH op for broker %s with "
+                           "should_fetch = %d",
+                           rd_kafka_broker_name(rkb),
+                           rko->rko_u.share_fetch.should_fetch);
+                /* This is only temporary handling for testing to avoid crashing
+                 * on assert  - the code below will automatically enqueue a
+                 * reply which is not the final behaviour. */
+                /* Insert errors randomly for testing, remove this code once
+                 * actual errors can be tested via the mock broker. */
+                // if (rd_jitter(0, 10) > 7) {
+                //         rd_rkb_dbg(rkb, CGRP, "SHAREFETCH",
+                //                    "Injecting error! %s : %d",
+                //                    rd_kafka_broker_name(rkb),
+                //                    rko->rko_u.share_fetch.should_fetch);
+
+                //         rd_kafka_op_reply(rko, RD_KAFKA_RESP_ERR__STATE);
+                //         rko = NULL;
+                // }
+                break;
+
         default:
                 rd_kafka_assert(rkb->rkb_rk, !*"unhandled op type");
                 break;

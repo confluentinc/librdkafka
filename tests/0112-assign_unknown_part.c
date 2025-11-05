a080ac7d18ab7d08678d689b65e9e454c399804f
Ankith-Confluent <anl@confluent.io>
2025-11-05 12:00:56 +0530

    flaky fixes

--- tests/0112-assign_unknown_part.c
+++ tests/0112-assign_unknown_part.c
@@ -52,7 +52,7 @@ int main_0112_assign_unknown_part(int argc, char **argv) {
         TEST_SAY("Creating topic %s with 1 partition\n", topic);
         test_create_topic_wait_exists(c, topic, 1, -1, tmout_multip(1000));
 
-        test_wait_for_metadata_propagation(3);
+        rd_sleep(4);
 
         TEST_SAY("Producing message to partition 0\n");
         test_produce_msgs_easy(topic, testid, 0, 1);
@@ -68,8 +68,7 @@ int main_0112_assign_unknown_part(int argc, char **argv) {
         TEST_SAY("Changing partition count for topic %s\n", topic);
         test_create_partitions(NULL, topic, 2);
 
-
-        test_wait_for_metadata_propagation(3);
+        rd_sleep(4);
 
         TEST_SAY("Producing message to partition 1\n");
         test_produce_msgs_easy(topic, testid, 1, 1);

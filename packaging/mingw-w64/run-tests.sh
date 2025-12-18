dc609c988ac9f98882b7d3dc53b1822dd5e44ee4
Emanuele Sabellico <esabellico@confluent.io>
2025-12-18 10:21:35 +0100

    Run KIP-848 tests on MinGW-w64

--- packaging/mingw-w64/run-tests.sh
+++ packaging/mingw-w64/run-tests.sh
@@ -3,4 +3,7 @@
 set -e
 
 cd tests
-./test-runner.exe -l -Q -p1 0000
+export TEST_CONSUMER_GROUP_PROTOCOL=classic
+./test-runner.exe -l -Q -p1
+export TEST_CONSUMER_GROUP_PROTOCOL=consumer
+./test-runner.exe -l -Q -p1 

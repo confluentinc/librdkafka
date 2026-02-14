7bf5ed096963c756cb140f2596eaacfb8faa729f
Raymond <raymond@anadon.eu>
2026-02-14 17:43:19 +0100

    Fix potential bug snprintf potential truncated str
    Increase buffer size for member_id in range assignor

--- src/rdkafka_range_assignor.c
+++ src/rdkafka_range_assignor.c
@@ -967,8 +967,8 @@ static int setupRackAwareAssignment0(rd_kafka_t *rk,
             rd_kafka_metadata_get_internal(*metadata));
 
         for (i = 0; i < member_cnt; i++) {
-                char member_id[10];
-                snprintf(member_id, 10, "consumer%d", (int)(i + 1));
+                char member_id[11];
+                snprintf(member_id, 11, "consumer%d", (int)(i + 1));
                 ut_init_member_with_rack(
                     &members[i], member_id, ALL_RACKS[consumer_racks[i]],
                     subscriptions[i], subscriptions_count[i]);

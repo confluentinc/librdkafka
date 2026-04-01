9335fd92275ef9cd8fd19dbc0ea7ff42de170249
PratRanj07 <pranjan@confluent.io>
2026-04-01 19:17:31 +0530

    Wrote tests for checking transactions and control records filtering happening properly

--- tests/test.c
+++ tests/test.c
@@ -294,6 +294,7 @@ _TEST_DECL(0171_share_consumer_consume);
 _TEST_DECL(0172_share_consumer_acknowledge);
 _TEST_DECL(0173_share_consumer_commit_async_local);
 _TEST_DECL(0173_share_consumer_commit_async);
+_TEST_DECL(0177_share_consumer_transactions);
 
 /* Manual tests */
 _TEST_DECL(8000_idle);
@@ -574,6 +575,7 @@ struct test tests[] = {
           TEST_F_LOCAL,
           TEST_BRKVER(0, 4, 0, 0)),
     _TEST(0173_share_consumer_commit_async, 0, TEST_BRKVER(0, 4, 0, 0)),
+    _TEST(0177_share_consumer_transactions, 0, TEST_BRKVER(0, 4, 0, 0)),
 
     /* Manual tests */
     _TEST(8000_idle, TEST_F_MANUAL),

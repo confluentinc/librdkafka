19634e388ce81e24af139a1812b57c69974101f2
Ankith L <anl@confluent.io>
2026-05-20 13:03:38 +0530

    Add end-to-end share consumer telemetry test and update build configurations

--- tests/test.c
+++ tests/test.c
@@ -309,6 +309,8 @@ _TEST_DECL(0181_share_consumer_topic_delete);
 _TEST_DECL(0182_share_consumer_error_handling_mock);
 _TEST_DECL(0183_share_consumer_leader_change_mock);
 _TEST_DECL(0185_share_consumer_max_poll_interval);
+_TEST_DECL(0185_share_consumer_max_poll_interval_local);
+_TEST_DECL(0190_share_consumer_telemetry_e2e);
 
 /* Manual tests */
 _TEST_DECL(8000_idle);
@@ -600,6 +602,9 @@ struct test tests[] = {
     _TEST(0182_share_consumer_error_handling_mock, TEST_F_LOCAL),
     _TEST(0183_share_consumer_leader_change_mock, TEST_F_LOCAL),
     _TEST(0185_share_consumer_max_poll_interval, 0, TEST_BRKVER(0, 4, 2, 0)),
+    _TEST(0185_share_consumer_max_poll_interval_local, TEST_F_LOCAL),
+    _TEST(0190_share_consumer_telemetry_e2e, TEST_F_MANUAL,
+          TEST_BRKVER(0, 4, 0, 0)),
 
     /* Manual tests */
     _TEST(8000_idle, TEST_F_MANUAL),

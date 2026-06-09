46d787238fb08b80dc3bf481d880aef757adf460
Ankith L <anl@confluent.io>
2026-06-09 17:05:20 +0530

    minor fix

--- tests/test.c
+++ tests/test.c
@@ -310,7 +310,6 @@ _TEST_DECL(0181_share_consumer_topic_delete);
 _TEST_DECL(0182_share_consumer_error_handling_mock);
 _TEST_DECL(0183_share_consumer_leader_change_mock);
 _TEST_DECL(0185_share_consumer_max_poll_interval);
-_TEST_DECL(0185_share_consumer_max_poll_interval_local);
 _TEST_DECL(0190_share_consumer_telemetry);
 
 /* Manual tests */
@@ -604,10 +603,9 @@ struct test tests[] = {
     _TEST(0182_share_consumer_error_handling_mock, TEST_F_LOCAL),
     _TEST(0183_share_consumer_leader_change_mock, TEST_F_LOCAL),
     _TEST(0185_share_consumer_max_poll_interval, 0, TEST_BRKVER(0, 4, 2, 0)),
-    _TEST(0185_share_consumer_max_poll_interval_local, TEST_F_LOCAL),
     _TEST(0190_share_consumer_telemetry,
           TEST_F_MANUAL,
-          TEST_BRKVER(0, 4, 0, 0)),
+          TEST_BRKVER(0, 4, 2, 0)),
 
     /* Manual tests */
     _TEST(8000_idle, TEST_F_MANUAL),

e50b4d2ed951a3ce21885063380fa85673236e3d
Pranav Shah <prashah@confluent.io>
2026-07-08 12:03:16 +0530

    Fix the parsing for cluster_id

--- src/rdkafka_metadata.c
+++ src/rdkafka_metadata.c
@@ -677,9 +677,14 @@ rd_kafka_parse_Metadata0(rd_kafka_broker_t *rkb,
         mdi->cluster_id = NULL;
         if (ApiVersion >= 2) {
                 rd_kafka_buf_read_str(rkbuf, &cluster_id);
-                if (cluster_id.str)
-                        mdi->cluster_id =
-                            rd_tmpabuf_write_str(&tbuf, cluster_id.str);
+                if (!RD_KAFKAP_STR_IS_NULL(&cluster_id)) {
+                        int clen        = RD_KAFKAP_STR_LEN(&cluster_id);
+                        mdi->cluster_id = rd_tmpabuf_alloc(&tbuf, clen + 1);
+                        if (mdi->cluster_id) {
+                                memcpy(mdi->cluster_id, cluster_id.str, clen);
+                                mdi->cluster_id[clen] = '\0';
+                        }
+                }
         }
 
         mdi->controller_id = -1;

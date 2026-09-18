f607a8cf7fc0c2ad6f88e9fe98572a2429f9bf57
Kaushik Raina <kraina@confluent.io>
2026-09-18 17:12:56 +0530

    Fix offfset commit skip during rebalancing state and max poll interval expire

--- src/rdkafka_cgrp.c
+++ src/rdkafka_cgrp.c
@@ -5578,6 +5578,8 @@ static void rd_kafka_cgrp_revoke_all_rejoin_maybe(rd_kafka_cgrp_t *rkcg,
                         : "",
                     rkcg->rkcg_rebalance_rejoin ? ", rejoin on rebalance" : "",
                     reason);
+                if (assignment_lost)
+                        rd_kafka_cgrp_assignment_set_lost(rkcg, "%s", reason);
                 return;
         }
 
